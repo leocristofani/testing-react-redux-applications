86c15e729aa57e1ce698a1614e8cfef46e0542a6
Leonardo <leo.cristofani@gmail.com>
2017-03-09 13:43:48 -0300

    add more action tests

--- src/user/action_creators/user_delete_action_creators.test.js
+++ src/user/action_creators/user_delete_action_creators.test.js
@@ -15,15 +15,36 @@ import {
 const mockStore = reduxMockStore([thunk]);
 
 describe('User delete action creators', () => {
-    it('should create action to inform user is being deleted');
-    /**
-     * Testing sync action creators
-     */
+
+    afterEach(() => {
+        nock.cleanAll();
+    });
+
+    it('should create action to inform user is being deleted', () => {
+        const userId = 123;
+        const expectedAction = {
+            type: DELETE_USER_REQUEST,
+            payload: { userId }
+        }
+        const actualAction = deleteUserRequest(userId);
+
+        expect(expectedAction).toEqual(actualAction);
+    });
+    
     it('should create action to inform user was deleted successfully');
     it('should create action to inform user was NOT deleted successfully');
-    /**
-     * Testing async action creators
-     */
-    it('should create action to delete user');
+
+    it('should create action to delete user', () => {
+        const user = { name: 'test', email: 'test@email.com', id: 123 };
+        nock('http://localhost').delete(`/api/users/${user.id}`).reply(200, user);
+        const store = mockStore({ user: { list: [user] } });
+        return store.dispatch(deleteUser(user.id))
+            .then(() => {
+                expect(store.getActions()).toEqual([
+                    { type: DELETE_USER_REQUEST, payload: { userId: user.id } },
+                    { type: DELETE_USER_SUCCESS, payload: { user } }
+                ]);
+            });
+    });
 
 });
